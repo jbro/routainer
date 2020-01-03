669375197ee864402324bd5d1b4dc1f8f385e408
Jesper B. Rosenkilde <jbr@humppa.dk>
2020-01-03 13:58:27 +0000

    Mount in and run install script

--- containers/update-debian.sh
+++ containers/update-debian.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 INCLUDE="systemd-container,kitty-terminfo,vim,bash-completion"
 DATE=$(date +%Y%m%d%H%M%S)
-debootstrap --include="$INCLUDE" stable "/media/root/var/lib/machines/debian-$DATE"
+IMAGEPATH="/media/root/var/lib/machines/debian-$DATE"
+
+debootstrap --include="$INCLUDE" stable "$IMAGEPATH"
+
+cp "/media/root/data/routainer/containers/routainer-install.service" "$IMAGEPATH/usr/lib/systemd/system/"
+chroot "$IMAGEPATH" systemctl enable routainer-install.service
+
+chroot "$IMAGEPATH" systemctl disable ifupdown-pre.service
+chroot "$IMAGEPATH" systemctl disable networking.service
+chroot "$IMAGEPATH" systemctl enable systemd-networkd.service
