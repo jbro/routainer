00bf283649b46ec1f653c9908f6bafc1a01af3b3
Jesper B. Rosenkilde <jbr@humppa.dk>
2020-01-03 16:21:14 +0000

    Enable NAT on router

--- containers/router/install.sh
+++ containers/router/install.sh
@@ -5,3 +5,6 @@ apt-get upgrade -y
 apt-get install -y dnsmasq
 cp /opt/routainer/linknet-dhcp.conf /etc/dnsmasq.d/
 systemctl restart dnsmasq
+
+echo 1 > /proc/sys/net/ipv4/ip_forward
+/sbin/iptables -t nat -A POSTROUTING -o enp1s0 -j MASQUERADE
