55cef8ee8c57ae73015e39c2bfa5616f3c613ea9
Jesper B. Rosenkilde <jbr@humppa.dk>
2020-01-04 13:33:49 +0000

    Enable logging
    remove password

--- containers/clientnet/install.sh
+++ containers/clientnet/install.sh
@@ -8,9 +8,8 @@ echo 1 > /proc/sys/net/ipv4/ip_forward
 
 apt-get install -y curl
 mkdir /etc/pihole
-#Gw10z7G8
 cp /opt/routainer/setupVars.conf /etc/pihole/setupVars.conf
 curl -sSL https://install.pi-hole.net | bash /dev/stdin --unattended
-pihole logging off
+#pihole logging off
 cp /opt/routainer/02-pihole-dhcp.conf /etc/dnsmasq.d/02-pihole-dhcp.conf
 pihole restartdns
