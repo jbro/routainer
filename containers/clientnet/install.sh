9e7757516615e0f7b4337e9b8e732af80b74b2e0
Jesper B. Rosenkilde <jbr@humppa.dk>
2020-01-04 09:29:54 +0000

    Pihole and stuff on the client network

--- containers/clientnet/install.sh
+++ containers/clientnet/install.sh
@@ -4,6 +4,11 @@ hostname clientnet
 apt-get update
 apt-get upgrade -y
 
-#apt-get install -y dnsmasq
-#cp /opt/routainer/linknet-dhcp.conf /etc/dnsmasq.d/
-#systemctl restart dnsmasq
+apt-get install -y curl
+mkdir /etc/pihole
+#Gw10z7G8
+cp /opt/routainer/setupVars.conf /etc/pihole/setupVars.conf
+curl -sSL https://install.pi-hole.net | bash /dev/stdin --unattended
+pihole logging off
+cp /opt/routainer/02-pihole-dhcp.conf /etc/dnsmasq.d/02-pihole-dhcp.conf
+pihole restartdns
