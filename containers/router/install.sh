fd3eadf5b000396643dde37836c68e7f5eea2eee
Jesper B. Rosenkilde <jbr@humppa.dk>
2020-01-04 09:42:50 +0000

    Static adressing on link net

--- containers/router/install.sh
+++ containers/router/install.sh
@@ -4,9 +4,6 @@ hostname router
 apt-get update
 apt-get upgrade -y
 
-apt-get install -y dnsmasq
-cp /opt/routainer/linknet-dhcp.conf /etc/dnsmasq.d/
-systemctl restart dnsmasq
-
 echo 1 > /proc/sys/net/ipv4/ip_forward
-/sbin/iptables -t nat -A POSTROUTING -o enp1s0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o enp1s0 -j MASQUERADE
+ip route add 192.168.200.0/24 via 192.168.100.200
