4970606dcb81bd3a45759a6918301c7547bfa7bc
Jesper B. Rosenkilde <jbr@humppa.dk>
2020-01-05 22:51:23 +0000

    Use dedicated dnscache and pihole for this network

--- containers/clientnet/install.sh
+++ containers/clientnet/install.sh
@@ -5,11 +5,3 @@ apt-get update
 apt-get upgrade -y
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
-
-apt-get install -y curl
-mkdir /etc/pihole
-cp /opt/routainer/setupVars.conf /etc/pihole/setupVars.conf
-curl -sSL https://install.pi-hole.net | bash /dev/stdin --unattended
-#pihole logging off
-cp /opt/routainer/02-pihole-dhcp.conf /etc/dnsmasq.d/02-pihole-dhcp.conf
-pihole restartdns
