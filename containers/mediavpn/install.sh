6b1c567243053c251402fee814588fa722ff71a2
Jesper B. Rosenkilde <jbr@humppa.dk>
2020-01-05 23:12:36 +0000

    Switch to systemd dhcpserver

--- containers/mediavpn/install.sh
+++ containers/mediavpn/install.sh
@@ -4,10 +4,7 @@ hostname mediavpn
 apt-get update
 apt-get upgrade -y
 
-mkdir /etc/dnsmasq.d/
-cp /opt/routainer/dhcp.conf /etc/dnsmasq.d/
-
-apt-get install -y dnsmasq openvpn openvpn-systemd-resolved
+apt-get install -y openvpn openvpn-systemd-resolved
 
 rmdir /etc/openvpn/client
 ln -sf /opt/vpn-configs /etc/openvpn/client
@@ -16,5 +13,6 @@ mkdir /etc/systemd/system/openvpn-client@.service.d
 cp /opt/routainer/override.conf /etc/systemd/system/openvpn-client@.service.d/
 
 systemctl start openvpn-client@my_expressvpn_usa_-_new_york_udp
+
 echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
