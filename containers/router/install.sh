d18b07fba85cca38e0356d31ef5f70af2d9dd9f5
Jesper B. Rosenkilde <jbr@humppa.dk>
2020-01-05 16:40:38 +0000

    Management network container + magic

--- containers/router/install.sh
+++ containers/router/install.sh
@@ -6,5 +6,7 @@ apt-get upgrade -y
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -t nat -A POSTROUTING -o enp1s0 -j MASQUERADE
-ip route add 192.168.200.0/24 via 192.168.100.200
-ip route add 192.168.202.0/24 via 192.168.100.202
+ip route add 192.168.0.0/24 via 192.168.100.10    # MGMT net
+ip route add 192.168.200.0/24 via 192.168.100.200 # Client net
+ip route add 192.168.202.0/24 via 192.168.100.202 # Guest net
+
