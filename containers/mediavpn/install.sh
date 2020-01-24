ce90b116f804a53bc17ce4b756ee5f43b48e8962
Jesper B. Rosenkilde <jbr@humppa.dk>
2020-01-24 22:00:28 +0000

    Auto start US vpn

--- containers/mediavpn/install.sh
+++ containers/mediavpn/install.sh
@@ -17,7 +17,6 @@ ln -sf /opt/vpn-configs /etc/openvpn/client
 mkdir /etc/systemd/system/openvpn-client@.service.d
 cp /opt/routainer/override.conf /etc/systemd/system/openvpn-client@.service.d/
 
-#systemctl start openvpn-client@my_expressvpn_usa_-_new_york_udp
+systemctl start openvpn-client@my_expressvpn_usa_-_new_york_udp
 
-#echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
