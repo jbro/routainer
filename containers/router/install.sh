85b2723cf6f70cca92ec175a6ccdce2eb4bc5d49
Jesper B. Rosenkilde <jbr@humppa.dk>
2020-03-31 17:04:27 +0000

    Setup basic firewall

--- containers/router/install.sh
+++ containers/router/install.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 hostname router
 
+iptables -P INPUT DROP
+
+iptables -A INPUT -i lo -j ACCEPT
+
+iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
+iptables -A INPUT -p icmp -m icmp --icmp-type 8 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A INPUT -p udp -j REJECT --reject-with icmp-port-unreachable
+iptables -A INPUT -p tcp -j REJECT --reject-with tcp-reset
+iptables -A INPUT -j REJECT --reject-with icmp-proto-unreachable
+
 echo 'nameserver 8.8.8.8' > /etc/resolv.conf
 echo 'nameserver 8.8.4.4' >> /etc/resolv.conf
 
@@ -9,7 +20,6 @@ apt-get upgrade -y
 
 echo 'nameserver 192.168.100.50' > /etc/resolv.conf
 
-# Start routing before we update so dnscache can bootstrap
 echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -t nat -A POSTROUTING -o enp1s0 -j MASQUERADE
 ip route add 192.168.0.0/24 via 192.168.100.10    # MGMT net
