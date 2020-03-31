14516d320d7d01044e6ae04f06492939c732bf08
Jesper B. Rosenkilde <jbr@humppa.dk>
2020-03-31 17:04:09 +0000

    Include less sofware as default

--- containers/update-debian.sh
+++ containers/update-debian.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-INCLUDE="systemd-container,kitty-terminfo,vim,bash-completion,dnsutils"
+INCLUDE="systemd-container,kitty-terminfo,iptables"
 DATE=$(date +%Y%m%d%H%M%S)
 RELEASE=stable
 IMAGEPATH="/media/root/var/lib/machines/debian-$RELEASE-$DATE"
 
-debootstrap --variant=minbase --include="$INCLUDE" $RELEASE "$IMAGEPATH"
+debootstrap --include="$INCLUDE" $RELEASE "$IMAGEPATH"
 
 cp "/media/root/data/routainer/containers/routainer-install.service" "$IMAGEPATH/usr/lib/systemd/system/"
 chroot "$IMAGEPATH" systemctl enable routainer-install.service
-
 chroot "$IMAGEPATH" systemctl enable systemd-networkd.service
