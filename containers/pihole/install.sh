afc1f44a43d5b4e36ad10141f82f6ce7fd6b987a
Jesper Brix Rosenkilde <jbr@humppa.dk>
2021-08-20 18:41:41 +0000

    Force install on Debian 11

--- containers/pihole/install.sh
+++ containers/pihole/install.sh
@@ -5,4 +5,4 @@ apt-get update
 apt-get upgrade -y
 
 apt-get install -y curl
-curl -sSL https://install.pi-hole.net | bash /dev/stdin --unattended
+curl -sSL https://install.pi-hole.net | PIHOLE_SKIP_OS_CHECK=true bash /dev/stdin --unattended
