eddaeb5bc92fd5d81a54c569d5fb4511dee4523f
Jesper Brix Rosenkilde <jbr@humppa.dk>
2021-09-11 08:13:44 +0000

    Add workaround for Debian 11

--- containers/pihole/install.sh
+++ containers/pihole/install.sh
@@ -4,5 +4,5 @@ hostname pihole
 apt-get update
 apt-get upgrade -y
 
-apt-get install -y curl
+apt-get install -y curl lighttpd
 curl -sSL https://install.pi-hole.net | PIHOLE_SKIP_OS_CHECK=true bash /dev/stdin --unattended
