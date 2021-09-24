89f516a26b01962ec1403d0b3bf4a5a64d394146
Jesper Brix Rosenkilde <jbr@humppa.dk>
2021-09-24 20:43:42 +0000

    Yet another workaround for pihole

--- containers/pihole/install.sh
+++ containers/pihole/install.sh
@@ -4,5 +4,7 @@ hostname pihole
 apt-get update
 apt-get upgrade -y
 
-apt-get install -y curl lighttpd
-curl -sSL https://install.pi-hole.net | PIHOLE_SKIP_OS_CHECK=true bash /dev/stdin --unattended
+apt-get install -y curl
+curl -sSL https://install.pi-hole.net | bash /dev/stdin --unattended
+setcap CAP_NET_BIND_SERVICE,CAP_NET_RAW,CAP_NET_ADMIN+ei /usr/bin/pihole-FTL
+/usr/bin/pihole-FTL
