050185fa07241715161e2d20e60591c92ef9bcf5
Jesper Brix Rosenkilde <jbr@humppa.dk>
2022-04-10 09:24:12 +0000

    Fix permissions on su
    needed to start pihole-FTL

--- containers/pihole/install.sh
+++ containers/pihole/install.sh
@@ -4,5 +4,6 @@ hostname pihole
 apt-get update
 apt-get upgrade -y
 
+chown -R root:root /bin/su ; chmod 755 /bin/su ; chmod u+s /bin/su
 apt-get install -y curl
 curl -sSL https://install.pi-hole.net | bash /dev/stdin --unattended
